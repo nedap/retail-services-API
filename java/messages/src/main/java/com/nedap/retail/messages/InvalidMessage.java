aae469568906f3c76d12d58491b95515135ffd62
jasperkremer <jasper.kremer@nedap.com>
2014-06-17 12:18:01 +0200

    [#72011282] Improve and add more API calls.

--- java/messages/src/main/java/com/nedap/retail/messages/InvalidMessage.java
+++ java/messages/src/main/java/com/nedap/retail/messages/InvalidMessage.java
@@ -2,14 +2,43 @@ package com.nedap.retail.messages;
 
 public class InvalidMessage extends Exception {
 
+    private static final long serialVersionUID = 1L;
+    /**
+     * Success! Message is successfully processed.
+     */
+    public final static int OK = 200;
+    /**
+     * The request cannot be fulfilled due to bad syntax. An accompanying error message will explain why.
+     * For example invalid JSON format.
+     */
+    public final static int BadRequest = 400;
+    /**
+     * Unauthorized access.
+     * Access token is expired or is invalid. A new access token must be acquired.
+     */
+    public final static int Unauthorized = 401;
+    /**
+     * Forbidden to access resource (invalid client-scopes and/or user-roles).
+     * Authorization will not help and the request SHOULD NOT be repeated.
+     */
+    public final static int Forbidden = 403;
+    /**
+     * Something is broken. Please post to the group so the team can investigate.
+     */
+    public final static int InternalError = 500;
+    /**
+     * Signature in message is not correct. This normally indicates a bug in the client-software.
+     */
+    public final static int SignatureError = 800;
+
     private final int status;
 
     public InvalidMessage(String string, Throwable thrwbl) {
-        this(ApiResponse.BadRequest, string, thrwbl);
+        this(BadRequest, string, thrwbl);
     }
 
     public InvalidMessage(String string) {
-        this(ApiResponse.BadRequest, string);
+        this(BadRequest, string);
     }
 
     public InvalidMessage(int status, String string, Throwable thrwbl) {
