cf104bbc28bb253de68b997b036f71f162c9321c
Erik Gustafsson <egustafsson@execom.eu>
2014-06-18 14:53:49 +0200

    [#72873242] Make equals method on lists of element types work

--- java/messages/src/main/java/com/nedap/retail/messages/epcis/v1_1/elements/BizTransactionElement.java
+++ java/messages/src/main/java/com/nedap/retail/messages/epcis/v1_1/elements/BizTransactionElement.java
@@ -1,7 +1,8 @@
 package com.nedap.retail.messages.epcis.v1_1.elements;
 
-import com.google.gson.annotations.SerializedName;
 import org.codehaus.jackson.annotate.JsonProperty;
+
+import com.google.gson.annotations.SerializedName;
 
 public class BizTransactionElement {
 
@@ -16,7 +17,7 @@ public class BizTransactionElement {
     public BizTransactionElement() {
     }
 
-    public BizTransactionElement(String type, String bizTransaction) {
+    public BizTransactionElement(final String type, final String bizTransaction) {
         this.type = type;
         this.bizTransaction = bizTransaction;
     }
@@ -24,5 +25,43 @@ public class BizTransactionElement {
     @Override
     public String toString() {
         return "BizTransactionElement: type=" + type + ",biz_transaction=" + bizTransaction;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (bizTransaction == null ? 0 : bizTransaction.hashCode());
+        result = prime * result + (type == null ? 0 : type.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final BizTransactionElement other = (BizTransactionElement) obj;
+        if (bizTransaction == null) {
+            if (other.bizTransaction != null) {
+                return false;
+            }
+        } else if (!bizTransaction.equals(other.bizTransaction)) {
+            return false;
+        }
+        if (type == null) {
+            if (other.type != null) {
+                return false;
+            }
+        } else if (!type.equals(other.type)) {
+            return false;
+        }
+        return true;
     }
 }
--- java/messages/src/main/java/com/nedap/retail/messages/epcis/v1_1/elements/SourceElement.java
+++ java/messages/src/main/java/com/nedap/retail/messages/epcis/v1_1/elements/SourceElement.java
@@ -1,7 +1,8 @@
 package com.nedap.retail.messages.epcis.v1_1.elements;
 
-import com.google.gson.annotations.SerializedName;
 import org.codehaus.jackson.annotate.JsonProperty;
+
+import com.google.gson.annotations.SerializedName;
 
 public class SourceElement {
 
@@ -16,7 +17,7 @@ public class SourceElement {
     public SourceElement() {
     }
 
-    public SourceElement(String type, String source) {
+    public SourceElement(final String type, final String source) {
         this.type = type;
         this.source = source;
     }
@@ -24,5 +25,43 @@ public class SourceElement {
     @Override
     public String toString() {
         return "SourceElement: type=" + type + ",source=" + source;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (source == null ? 0 : source.hashCode());
+        result = prime * result + (type == null ? 0 : type.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SourceElement other = (SourceElement) obj;
+        if (source == null) {
+            if (other.source != null) {
+                return false;
+            }
+        } else if (!source.equals(other.source)) {
+            return false;
+        }
+        if (type == null) {
+            if (other.type != null) {
+                return false;
+            }
+        } else if (!type.equals(other.type)) {
+            return false;
+        }
+        return true;
     }
 }
