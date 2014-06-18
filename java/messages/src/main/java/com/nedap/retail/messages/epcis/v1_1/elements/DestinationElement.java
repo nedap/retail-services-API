33b3fd37e34d2b7de14a6eabc6321626126942e9
Erik Gustafsson <egustafsson@execom.eu>
2014-06-18 14:53:49 +0200

    [#72873242] Make equals method on lists of element types work

--- java/messages/src/main/java/com/nedap/retail/messages/epcis/v1_1/elements/DestinationElement.java
+++ java/messages/src/main/java/com/nedap/retail/messages/epcis/v1_1/elements/DestinationElement.java
@@ -1,7 +1,8 @@
 package com.nedap.retail.messages.epcis.v1_1.elements;
 
-import com.google.gson.annotations.SerializedName;
 import org.codehaus.jackson.annotate.JsonProperty;
+
+import com.google.gson.annotations.SerializedName;
 
 public class DestinationElement {
 
@@ -16,7 +17,7 @@ public class DestinationElement {
     public DestinationElement() {
     }
 
-    public DestinationElement(String type, String destination) {
+    public DestinationElement(final String type, final String destination) {
         this.type = type;
         this.destination = destination;
     }
@@ -24,5 +25,43 @@ public class DestinationElement {
     @Override
     public String toString() {
         return "DestinationElement: type=" + type + ",destination=" + destination;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (destination == null ? 0 : destination.hashCode());
+        result = prime * result + (type == null ? 0 : type.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final DestinationElement other = (DestinationElement) obj;
+        if (destination == null) {
+            if (other.destination != null) {
+                return false;
+            }
+        } else if (!destination.equals(other.destination)) {
+            return false;
+        }
+        if (type == null) {
+            if (other.type != null) {
+                return false;
+            }
+        } else if (!type.equals(other.type)) {
+            return false;
+        }
+        return true;
     }
 }
