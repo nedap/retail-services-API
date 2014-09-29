db7d93d2bdcdbba83d01e54349297ec67fb90b6a
jasperkremer <jasper.kremer@nedap.com>
2014-09-29 11:34:24 +0200

    [#76500190] Added endpoint /users/1.0/get (fixed unit test)

--- java/messages/src/test/java/com/nedap/retail/messages/users/UserTest.java
+++ java/messages/src/test/java/com/nedap/retail/messages/users/UserTest.java
@@ -7,6 +7,9 @@ import static org.junit.Assert.assertTrue;
 import java.util.Set;
 
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.joda.time.DateTime;
+import static org.junit.Assert.assertNull;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,7 +22,7 @@ public class UserTest {
 
     @Before
     public void setUp() {
-        mapper = new ObjectMapper();
+        mapper = new ObjectMapper().configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
     }
 
     @Test
@@ -32,17 +35,6 @@ public class UserTest {
         // Parse JSON into user object.
         final User user = mapper.readValue(json.toString(), User.class);
 
-        "expires_at":"2049-01-01T00:00:00Z"
-        
-        // {"id":"1","display_name":"emil kappert",
-        // "name":{"given_name":"emil","family_name":"kappert"},"gender":"m","sitegroup_id":1,
-        // "username":"emil.kappert","email":"emil.kappert@nedap.com","language":"en",
-        // "authorisations":[{"role":"cube_nedap_admin","expires_at":"2049-01-01T00:00:00Z"},
-        // {"role":"cube_back_office","expires_at":"2049-01-01T00:00:00Z"},{"role":"cube_user_manager","expires_at":null},
-        // {"role":"access_viewer","expires_at":null},{"role":"access_admin","expires_at":null},{"role":"access_admin_plus","expires_at":null},{"role":"access_super_admin","expires_at":null},{"role":"eas_viewer","expires_at":null},{"role":"visitor_viewer","expires_at":null},{"role":"light_energy_viewer","expires_at":null},{"role":"light_energy_super_admin","expires_at":null},{"role":"visuals_viewer","expires_at":null},{"role":"access_tech_level1","expires_at":"2049-01-01T00:00:00Z"},{"role":"access_tech_level2","expires_at":"2049-01-01T00:00:00Z"},{"role":"eas_tech_level1","expires_at":"2049-01-01T00:00:00Z"},{"role":"eas_tech_level2","expires_at":"2049-01-01T00:00:00Z"},{"role":"storeid_tech_level1","expires_at":"2049-01-01T00:00:00Z"},{"role":"storeid_tech_level2","expires_at":"2049-01-01T00:00:00Z"},{"role":"storeid_system_integrator","expires_at":"2049-01-01T00:00:00Z"},{"role":"storeid_system_admin","expires_at":null},{"role":"storeid_customer","expires_at":null},{"role":"storeid_customer_admin","expires_at":null},{"role":"isense_tech_level1","expires_at":"2049-01-01T00:00:00Z"},{"role":"isense_tech_level2","expires_at":"2049-01-01T00:00:00Z"},{"role":"isense_system_integrator","expires_at":"2049-01-01T00:00:00Z"},{"role":"isense_system_admin","expires_at":null},{"role":"isense_customer","expires_at":null},{"role":"isense_customer_admin","expires_at":null},{"role":"tweet_viewer","expires_at":null},{"role":"tweet_super_admin","expires_at":null},{"role":"cube_beta","expires_at":null},{"role":"multi_store","expires_at":null},{"role":"expert_mode","expires_at":null},{"role":"access_pin_and_block","expires_at":null}]}
-        
-        
-        
         // Test user object.
         assertEquals("1", user.getId());
         assertEquals("Bob", user.getDisplayName());
@@ -65,6 +57,61 @@ public class UserTest {
         assertTrue(user.isAuthorized("cube_back_office"));
         assertFalse(user.isAuthorized("cube_user_manager"));
         assertTrue(user.isAuthorized("foo"));
+    }
+
+    @Test
+    public void test_parse_json_user_expires_at() throws Exception {
+
+        final StringBuilder json = new StringBuilder();
+        json.append("{\"id\":\"1\",\"display_name\":\"emil kappert\"");
+        json.append(",\"given_name\":\"emil\",\"family_name\":\"kappert\"");
+        json.append(",\"gender\":\"m\",\"username\":\"emil.kappert\"");
+        json.append(",\"email\":\"emil.kappert@nedap.com\",\"language\":\"en\"");
+        json.append(",\"authorisations\":[{\"role\":\"cube_nedap_admin\"}");
+        json.append(",{\"role\":\"cube_back_office\",\"expires_at\":\"2014-01-02T00:00:00Z\"}");
+        json.append(",{\"role\":\"foo\",\"expires_at\":\"2015-02-03T00:00:00+01:00\"}]}");
+
+        // Parse JSON into user object.
+        final User user = mapper.readValue(json.toString(), User.class);
+        assertEquals("1", user.getId());
+        assertEquals("emil kappert", user.getDisplayName());
+        assertEquals("m", user.getGender());
+        assertEquals("en", user.getLanguage());
+        assertEquals("emil.kappert", user.getUsername());
+
+        // Number of authorisations should be 3, including role "foo"!
+        final Set<Authorisation> authorisations = user.getAuthorisations();
+        assertEquals(3, authorisations.size());
+
+        final Authorisation authorisation1 = getAuthorisation(authorisations, "cube_back_office");
+        assertEquals(new DateTime("2014-01-02T00:00:00Z").getMillis(), authorisation1.getExpiresAt().getMillis());
+        assertEquals("cube_back_office", authorisation1.getRole());
+        final Authorisation authorisation2 = getAuthorisation(authorisations, "foo");
+        assertEquals(new DateTime("2015-02-03T00:00:00+01:00").getMillis(), authorisation2.getExpiresAt().getMillis());
+        assertEquals("foo", authorisation2.getRole());
+        final Authorisation authorisation3 = getAuthorisation(authorisations, "cube_nedap_admin");
+        assertEquals("cube_nedap_admin", authorisation3.getRole());
+        assertNull(authorisation3.getExpiresAt());
+
+        // Test isAuthorized using role enum.
+        assertTrue(user.isAuthorized(Role.cube_nedap_admin));
+        assertTrue(user.isAuthorized(Role.cube_back_office));
+        assertFalse(user.isAuthorized(Role.cube_user_manager));
+
+        // Test isAuthorized using role string value.
+        assertTrue(user.isAuthorized("cube_nedap_admin"));
+        assertTrue(user.isAuthorized("cube_back_office"));
+        assertFalse(user.isAuthorized("cube_user_manager"));
+        assertTrue(user.isAuthorized("foo"));
+    }
+
+    private Authorisation getAuthorisation(final Set<Authorisation> authorisations, final String role) {
+        for (final Authorisation authorisation : authorisations) {
+            if (authorisation.getRole().equals(role)) {
+                return authorisation;
+            }
+        }
+        return null;
     }
 
     private static boolean containsRole(final Set<Authorisation> authorisations, final String role) {
