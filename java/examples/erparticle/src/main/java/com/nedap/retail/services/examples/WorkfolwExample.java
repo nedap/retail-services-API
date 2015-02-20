e89733f7d561344bf654b39ac2f8584a9cd97573
Nenad Milovanov <nmilovanov@execom.eu>
2015-02-20 16:18:42 +0100

    [#75318016] Added WorkflowExample.

---
+++ java/examples/erparticle/src/main/java/com/nedap/retail/services/examples/WorkfolwExample.java
@@ -0,0 +1,64 @@
+package com.nedap.retail.services.examples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+import com.nedap.retail.messages.Client;
+import com.nedap.retail.messages.workflow.WorkflowEvent;
+import com.sun.jersey.api.client.UniformInterfaceException;
+
+public class WorkfolwExample {
+
+    public static void runExample(final Client client) {
+        System.out.println("-------------");
+        System.out.println("Workflow API example");
+        System.out.println("-------------");
+
+        final String location = "http://location-testing";
+        final WorkflowEvent workflow = makeWorkflowEvent(location);
+
+        try {
+            // capture
+            System.out.println("------------- Capture workflow events");
+            client.captureWorkflow(workflow);
+            System.out.println("Captured workflow events");
+
+        } catch (final UniformInterfaceException e) {
+            System.out.println("Server responded with an error:");
+            System.out.println(e.getResponse().getEntity(String.class));
+        }
+
+        System.out.println("------------- Done");
+    }
+
+    private static WorkflowEvent makeWorkflowEvent(final String location) {
+        final WorkflowEvent workflow = new WorkflowEvent();
+
+        workflow.setType("cycle_count_started");
+        workflow.setEventTime(DateTime.now());
+        workflow.setLocation(location);
+        workflow.setEpcCount((long) 10);
+        workflow.setMessageIds(getMessageIds());
+
+        return workflow;
+    }
+
+    private static List<String> getMessageIds() {
+        final List<String> messageIds = new ArrayList<String>();
+
+        messageIds.add("1");
+        messageIds.add("2");
+        messageIds.add("3");
+        messageIds.add("4");
+        messageIds.add("5");
+        messageIds.add("6");
+        messageIds.add("7");
+        messageIds.add("8");
+        messageIds.add("9");
+        messageIds.add("10");
+
+        return messageIds;
+    }
+}
