a40305e057d67fac23c17b98019207c606f1013c
Marjan Stojanovic <mstojanovic@execom.eu>
2015-05-25 14:36:32 +0200

    Moved delete requests to commons

--- java/messages/src/main/java/com/nedap/retail/messages/epcis/v1_1/EpcisDeleteRequest.java
+++
@@ -1,7 +0,0 @@
-package com.nedap.retail.messages.epcis.v1_1;
-
-public class EpcisDeleteRequest {
-
-    public String location;
-    public String eventType;
-}
--- java/messages/src/main/java/com/nedap/retail/messages/workflow/WorkflowDeleteRequest.java
+++
@@ -1,7 +0,0 @@
-package com.nedap.retail.messages.workflow;
-
-
-public class WorkflowDeleteRequest {
-
-    public String location;
-}
