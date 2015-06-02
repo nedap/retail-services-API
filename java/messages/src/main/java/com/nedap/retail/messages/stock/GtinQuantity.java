defdd9b26fb5062623ac46d53ce78dd57558db44
spacemonkeythe <mladen.meseldzija@gmail.com>
2015-06-02 14:19:38 +0200

    Add exclude field to GtinQuantity.

--- java/messages/src/main/java/com/nedap/retail/messages/stock/GtinQuantity.java
+++ java/messages/src/main/java/com/nedap/retail/messages/stock/GtinQuantity.java
@@ -4,18 +4,24 @@ public class GtinQuantity {
 
     public String gtin;
     public int quantity;
+    public boolean exclude;
 
     public GtinQuantity() {
     }
-
+    
     public GtinQuantity(final String gtin, final int quantity) {
+    	this(gtin, quantity, false);
+    }
+
+    public GtinQuantity(final String gtin, final int quantity, boolean exclude) {
         this.gtin = gtin;
         this.quantity = quantity;
+        this.exclude = exclude;
     }
 
     @Override
     public String toString() {
-        return "GtinQuantity{" + "gtin=" + gtin + ", quantity=" + quantity + '}';
+        return "GtinQuantity{" + "gtin=" + gtin + ", quantity=" + quantity + "exclude=" + exclude + '}';
     }
 
     @Override
@@ -24,6 +30,7 @@ public class GtinQuantity {
         int result = 1;
         result = prime * result + (gtin == null ? 0 : gtin.hashCode());
         result = prime * result + quantity;
+        result = prime * result + (exclude ? 1 : 0);
         return result;
     }
 
@@ -49,6 +56,12 @@ public class GtinQuantity {
         if (quantity != other.quantity) {
             return false;
         }
+        
+        if (exclude != other.exclude) {
+        	return false;
+        }
+        
+        
         return true;
     }
 }
