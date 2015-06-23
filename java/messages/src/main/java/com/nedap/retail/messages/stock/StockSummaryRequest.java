ab2c56d1d0c598478d389c5d90a67850df0f9f3c
Marjan Stojanovic <mstojanovic@execom.eu>
2015-06-23 14:02:14 +0200

    [#95523714] Added new Request object for retrieve as it is only retrieve ERP endpoint that needs with_excluded field

--- java/messages/src/main/java/com/nedap/retail/messages/stock/StockSummaryRequest.java
+++ java/messages/src/main/java/com/nedap/retail/messages/stock/StockSummaryRequest.java
@@ -1,14 +1,7 @@
 package com.nedap.retail.messages.stock;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class StockSummaryRequest {
 
-    private final String INCLUDE_EXCLUDED = "include_excluded";
-
     public String id;
-
-    @JsonProperty(INCLUDE_EXCLUDED)
-    public boolean includeExcluded = false;
 
 }
