40a8314a578a5597bc0eae7fd3373a66975ede08
jasperkremer <jasper.kremer@nedap.com>
2015-07-16 10:59:00 +0200

    [#99206938] Don't fail on unknown properties when deserializing JSON

--- java/messages/src/main/java/com/nedap/retail/messages/IdCloudObjectMapperFactory.java
+++ java/messages/src/main/java/com/nedap/retail/messages/IdCloudObjectMapperFactory.java
@@ -1,9 +1,7 @@
 package com.nedap.retail.messages;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 
 public class IdCloudObjectMapperFactory {
@@ -13,6 +11,8 @@ public class IdCloudObjectMapperFactory {
         mapper.registerModule(new JodaModule());
         // Configure the object mapper to serialize dates according to the ISO-8601 standard
         mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        // Don't fail on unkonwn properties (Feature is enabled by default).
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         mapper.setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
         mapper.setSerializationInclusion(Include.NON_NULL);
         return mapper;
