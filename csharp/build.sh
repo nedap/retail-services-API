0755a9540d2492c78061cb18acbbc89747961d2a
bmjansen <bas.jansen@nedap.com>
2019-03-28 13:41:14 +0100

    Remove old stuff and add message about developer portal

--- csharp/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-export TERM=xterm
-rm -rf target &> /dev/null
-sh client/bin/generate.sh
-(cd target/generated-sources && exec sh build.sh)
-cp -f target/generated-sources/bin/* target/
-(cd examples && exec sh build.sh)
